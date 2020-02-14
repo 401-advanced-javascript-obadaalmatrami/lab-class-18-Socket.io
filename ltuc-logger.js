8f93c84e3a660170b8c4c3b2956b7ac2c658625b
obada almatrami <obada.matrami92@gmail.com>
2020-02-15 01:56:52 +0200

    finish

---
+++ ltuc-logger.js
@@ -0,0 +1,9 @@
+
+
+const sio = require('socket.io-client');
+
+const ltuc = sio.connect('http://localhost:3001/ltuc');
+
+ltuc.on('message', payload => {
+  console.log('LTUC message: ', payload);
+});
